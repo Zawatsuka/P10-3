d5ec65cc9fa46860fff791b1a3821c6f105efacc
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2021-01-12 10:03:16 +0100

    Amelioration

---
+++ varTab.php
@@ -0,0 +1,11 @@
+<?php
+$descriptVic = "Victor Hugo (Écouter) est un poète, dramaturge, écrivain, romancier et dessinateur romantique français, né le 7 ventôse an X (26 février 1802) à Besançon et mort le 22 mai 1885 à Paris. ";
+$descriptJean = "Jean de La Fontaine, né le 8 juillet 1621 à Château-Thierry et mort le 13 avril 1695 à Paris, est un poète français de grande renommée, principalement pour ses Fables et dans une moindre mesure pour ses contes.";
+$descriptPierre = "Pierre Corneille, aussi appelé « le Grand Corneille » ou « Corneille l'aîné », né le 6 juin 1606 à Rouen et mort le 1er octobre 1684 à Paris (paroisse Saint-Roch), est un dramaturge et poète français du xviie siècle.";
+$descriptJR = "Issu d'une famille de petits notables de la Ferté-Milon et tôt orphelin, Racine reçoit auprès des « Solitaires » de Port-Royal une éducation littéraire et religieuse rare. Se détournant d'une carrière ecclésiastique, il entreprend, jeune, de faire une carrière des lettres1, en privilégiant la poésie et le théâtre tragique.";
+$portrait1 = array('name'=>'Victor', 'firstname'=>'Hugo', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg');
+$portrait2 = array('name'=>'Jean', 'firstname'=>'de La Fontaine', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/e/e1/La_Fontaine_par_Rigaud.jpg');
+$portrait3 = array('name'=>'Pierre', 'firstname'=>'Corneille', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/2/2a/Pierre_Corneille_2.jpg');
+$portrait4 = array('name'=>'Jean', 'firstname'=>'Racine', 'portrait'=>'http://upload.wikimedia.org/wikipedia/commons/d/d5/Jean_racine.jpg');
+
+?>
